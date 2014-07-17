c281ac925535228379c3df0072d66db6e3d2e3a4
DelS <del.b.smith@gmail.com>
2014-07-17 10:30:40 +1000

    Corrected draft
    Agrees with Matlab and Excel sample

--- pb/PassingBablok.py
+++ pb/PassingBablok.py
@@ -5,68 +5,6 @@ Created on 14 Jul 2014
 '''
 import argparse, math
 import csv
-'''
----------------------------------------------------------------
- generate a table of all (N*(N-1)/2)  distinct pairs of values and their difference
-'''
-def combine(Vector):
-    CV = []
-    for i in range(len(Vector)-1):
-        for j in range(i+1,len(Vector)):
-            CV.append ( [ Vector[i],Vector[j],Vector[j]-Vector[i] ] )
-    return CV
-
-'''
----------------------------------------------------------------
- find the median of a list of numbers
- sort them and return the value in the middle
- if there are an even number, average the two middle values
-'''
-def median( DataList ):
-    temp = DataList
-    temp.sort()
-    Num = len(temp)
-    if Num % 2:
-        return temp[(Num-1)/2]   # odd
-    else:
-        return (temp[Num/2-1]+temp[Num/2])/2.0
-
-'''
----------------------------------------------------------------
- validate each row of data points, removing incomplete entries
-'''
-def validate( Label, Var1, Var2 ):
-    for i in range(len(Label)-1,-1,-1):
-        if not Var1[i] or Var1[i] == 0 or not Var2[i] or Var2[i] == 0 :
-            print "Removing:", Label.pop(i), Var1.pop(i), Var2.pop(i)
-    return (Label, Var1, Var2)
-
-'''
----------------------------------------------------------------
- calculate the slope of the line between each pair of points
- ignore pairs that are identical
- exclude slopes equal to -1
- sort slopes in ascending order
- return the table of slopes, and the number of values less than -1
-'''
-def calc_slopes(Var1,Var2):
-    SV=[]
-    K=0
-    for i in range(len(Var1)):
-        d2 = Var2[i][2]
-        d1 = Var1[i][2]
-        if (d1 != 0 or d2 != 0) and (d1 != -d2):
-            try:
-                Slope = d2/d1
-            except:
-                if d2 > 0:
-                    Slope = 1.e+23 
-                else: 
-                    Slope = -1.e+23
-            K += (Slope < -1)
-            SV.append(Slope)
-    SV.sort()
-    return (SV,K)
 
 '''
  ------------------------------------------------------
@@ -104,9 +42,7 @@ def setup_args():
     return (File,Label,Var1,Var2)
 
 def read_results(File,Label,Name1, Name2):
-    ID = []
-    Var1 = []
-    Var2 = []
+    Points=[]
     with open(File, 'rU') as f:
         reader = csv.DictReader(f)
         for row in reader:
@@ -115,59 +51,101 @@ def read_results(File,Label,Name1, Name2):
             v1 = row.get(Name1)
             v2 = row.get(Name2)
             if v1 != '' and v2 != '':
-                ID.append(row.get(Label))
-                Var1.append(float(v1))
-                Var2.append(float(v2))
-
-    return (ID,Var1,Var2)
+                Points.append( ( row.get(Label), float(v1), float(v2) ) )
+    return Points
 
 '''
 ------------------------------------------------------------
- pick the LB & UB of Slope 
+ calculate the index of the upper and lower confidence bounds
 '''
-def slope_CI(SSlope,n,N,K):
+def calc_CI(SSlope,K,n):
     w = 1.96
+    N = len(SSlope)
     CI = w * math.sqrt( (n*(n-1)*(2*n+5))/18 )
-    M1 = int(round((N-CI)/2)) 
-    M2 = N - M1
+    M1 = int(round((N-CI)/2))     #pythen lists are 0-based
+    M2 = N-M1-1
     print "N: %d,  n: %d,  CI:  %d,  M1:  %d, M2:  %d, K:  %d" % (N,n,CI,M1,M2,K)
     
-    result = (SSlope[K+M1], SSlope[K+M2])
-    #print "UB: %f,  LB: %f" % result
-    
-    return result
+    return (M1,M2)
 
 '''
 --------------------------------------------------------------------------------
   calculate intercept given slope
 '''
-def calc_intercept(Var1,Var2,Slope):
-    AV = []
-    for i in range(len(Var1)):
-        AV.append( Var2[i] - Slope * Var1[i] )
-    return median(AV)
+def calc_intercept(Points,Slope):
+    temp = []
+    for i in range(len(Points)):
+        temp.append( Points[i][2] - Slope * Points[i][1] )
+    return median(temp)
+
+
+
+'''
+---------------------------------------------------------------
+ find the median of a list of numbers
+ sort them and return the value in the middle
+ if there are an even number, average the two middle values
+'''
+def median( DataList ):
+    temp = DataList
+    temp.sort()
+    Num = len(temp)
+    if Num % 2:
+        return temp[(Num-1)/2]   # odd
+    else:
+        return (temp[Num/2-1]+temp[Num/2])/2.0
+
+'''
+---------------------------------------------------------------
+ calculate the slope of the line between each pair of points
+ ignore pairs that are identical
+ exclude slopes equal to -1
+ sort slopes in ascending order
+ return the table of slopes, and the number of values less than -1
+'''
+def calc_slopes(Points):
+    # Points is a list of 3-tuples (ID,Var1,Var2)
+    def calc(Pt1,Pt2):
+        dy = Pt2[2]-Pt1[2]
+        dx = Pt2[1]-Pt1[1]
+        if dx != 0:
+            slope = dy/dx
+        elif dy < 0:
+            slope = -1.e+23
+        elif dy > 0:
+            slope = 1.e+23
+        else:
+            slope = None    
+        return slope
+    
+    SV = []
+    K=0
+    for i in range(len(Points)-1):
+        for j in range(i+1,len(Points)):
+            slope = calc(Points[i],Points[j])
+            if slope != None:
+                SV.append ( slope )
+                K += (slope < -1)
+    return ( SV.sort(),K )
 
 '''
  --------------------------------------------------------------------------------
 This is the Passing-Bablok analysis process
  -------------------------------------------------------------------------------- 
 '''
-def Passing_Bablok(Var1,Var2):
-    # enumerate all pairs of each measurand with their difference
-    CV1 = combine(Var1)
-    CV2 = combine(Var2)
-    
+def Passing_Bablok(PtTable):
     # calculate Slopes for all pairs of Var1,Var2 : estimated Slope is the median
-    (SV,K) = calc_slopes(CV1,CV2)
-    Beta_Est = median(SV[K:])
-    
-    # find the confidence limits on the slope
-    (Beta_LB,Beta_UB) = slope_CI(SV,len(Var1),len(SV),K)
-    
+    (SV,K) = calc_slopes(PtTable)
+
+    # find the estimated slope and confidence limits
+    M0 = len(SV) / 2.
+    (M1,M2) = calc_CI ( SV,K,len(PtTable) )
+    (Beta_Est,Beta_LB,Beta_UB) = (SV[M0],SV[M1],SV[M2])
+
     # calculate the intercept
-    Alpha_Est = calc_intercept(Var1, Var2, Beta_Est)
-    Alpha_LB = calc_intercept(Var1, Var2, Beta_UB)
-    Alpha_UB = calc_intercept(Var1, Var2, Beta_LB)
+    Alpha_Est = calc_intercept(PtTable, Beta_Est)
+    Alpha_LB = calc_intercept(PtTable, Beta_UB)
+    Alpha_UB = calc_intercept(PtTable, Beta_LB)
     
     return (Beta_Est,Beta_LB,Beta_UB),(Alpha_Est,Alpha_LB,Alpha_UB)
     
@@ -185,13 +163,10 @@ if __name__ == '__main__':
     print "Var1= %s,  Var2= %s" % (Name1,Name2)
     
     # initialise data arrays from stdin (file) in .csv format
-    (ID, V1, V2) = read_results(File, Label, Name1, Name2)
+    PtTable = read_results(File, Label, Name1, Name2)
     
-    # remove samples where either Variable is Undefined
-    (ID,V1,V2) = validate(ID,V1,V2)
-
     # compute the slope and intercept (and CI of each) by Passing-Bablok LR
-    (Beta_Est,Beta_LB,Beta_UB),(Alpha_Est,Alpha_LB,Alpha_UB) = Passing_Bablok(V1,V2)
+    (Beta_Est,Beta_LB,Beta_UB),(Alpha_Est,Alpha_LB,Alpha_UB) = Passing_Bablok(PtTable)
     print "Slope:     %10.6f\t  LB:  %10.6f\t   UB:  %10.6f" % (Beta_Est,Beta_LB,Beta_UB)
     print "Intercept  %10.6f\t  UB:  %10.6f\t   LB:  %10.6f" % (Alpha_Est,Alpha_UB,Alpha_LB)
     
